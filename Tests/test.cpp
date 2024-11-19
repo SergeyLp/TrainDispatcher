70cb73a7019f79c2b3ad4765899ab86080d13724
Sergey Lipatov <serreg123@gmail.com>
2024-11-19 20:45:51 +0300

    Add some test

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -2,13 +2,31 @@
 //PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "../Core/Railway.h"
 
-TEST(TestCaseCtors, Ctors) {
+TEST(GloabalTest, Ctors) {
   Railway railway{};
   Line l1, l2;
   Twain tw1;
   Station st1;
   Section sect1;
-  Block bl1;
+  Block bl1("test", 0, 10);
+
+  SUCCEED();
+}
+
+TEST(SectionTest, Append) {
+  Section sect1;
+
+  Block bl1("test", 0, 10);
+  Block bl2("test", 0, 10);
+  Block bl3("test", 0, 10);
+  Block bl4("test", 0, 10);
+  Block bl5("test", 0, 10);
+  Block bl6("test", 0, 10);
+  Block bl7("test", 0, 10);
+  Block bl8("test", 0, 10);
+
+  sect1.append(bl1);
+  sect1.append(bl2);
 
   SUCCEED();
 }
